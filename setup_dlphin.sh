21e429d8a8afeee53188ef8c158f37adfab39594
Brandon Chiarito <bchiarito@gmail.com>
2021-05-03 16:37:05 -0400

    dlphin setup script

---
+++ setup_dlphin.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+mkdir $1
+cd $1
+export SCRAM_ARCH=slc7_amd64_gcc700
+cmsrel CMSSW_10_2_22
+cd CMSSW_10_2_22/src
+cmsenv
+git cms-addpkg SimCalorimetry/HcalTrigPrimProducers
+git cms-addpkg SimCalorimetry/HcalTrigPrimAlgos
+git clone git@github.com:bchiarito/cmssw_dlphin_tpana.git MyAna
+scram build
+cd ../test/
+git init
+git remote add origin git@github.com:bchiarito/cmssw_dlphin_tptest.git
+git pull origin master
+ls -l
